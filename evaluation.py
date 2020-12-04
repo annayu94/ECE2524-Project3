0f4ba7ad3945fe0fca29724dc35034147151a13c
Anna Yu <annayu94@vt.edu>
2020-12-03 19:50:39 -0500

    Adds cell movement calculatoin

--- evaluation.py
+++ evaluation.py
@@ -9,3 +9,39 @@ EVENTS = [ pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_UP}),
            pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_RIGHT}),
            pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_DOWN}),
            pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_LEFT})]
+
+# define movement of cells
+CELLS = [ [(row, column) for column in range(4) for row in range(4)],                # UP
+          [(row, column) for row in range(4) for column in range(4 - 1, -1, -1)],    # RIGHT
+          [(row, column) for column in range(4) for row in range(4 - 1, -1, -1)],    # DOWN
+          [(row, column) for row in range(4) for column in range(4)]]                # LEFT
+
+GET_DELTAS = [ lambda row, column: ((i, column) for i in range(row + 1, 4)),        # UP
+               lambda row, column: ((row, i) for i in range(column - 1, -1, -1)),   # RIGHT
+               lambda row, column: ((i, column) for i in range(row - 1, -1, -1)),   # DOWN
+               lambda row, column: ((row, i) for i in range(column + 1, 4))]        # LEFT
+
+# Calculate empty cells
+def empty_cells(grid):
+    return [(x, y) for x in range(4) for y in range(4) if not grid[y][x]]
+
+def move(grid, action):
+    moved = 0
+    sum = 0
+    for row, column in CELLS[action]:
+        for delta_r, delta_c in GET_DELTAS[action](row, column):
+            # When the current cell is empty, but desired moving cell has value:
+            if not grid[row][column] and grid[delta_r][delta_c]:
+                # Move cell to the current title
+                grid[row][column], grid[delta_r][delta_c] = grid[delta_r][delta_c], 0
+                moved += 1
+            if grid[delta_r][delta_c]:
+                # When desired moving cell can be merged with the current cell
+                if grid[row][column] == grid[delta_r][delta_c]:
+                    grid[row][column] *= 2
+                    grid[delta_r][delta_c] = 0
+                    sum += grid[row][column]
+                    moved+=1
+                # When stop moving
+                break
+    return grid, moved, sum
